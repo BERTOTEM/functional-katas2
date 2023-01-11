cdb2a7af6d263868580a635b020b8ca236962ca9
JULIAN MAZO <82735837+BERTOTEM@users.noreply.github.com>
2023-01-11 18:04:15 -0500

    Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url     DataSource: DataUtil.getMovies()     Output: List of ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl")

--- src/main/java/katas/Kata9.java
+++ src/main/java/katas/Kata9.java
@@ -2,14 +2,13 @@ package katas;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
-import model.Bookmark;
-import model.Movie;
-import model.MovieList;
+import model.*;
 import util.DataUtil;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: Retrieve each video's id, title, middle interesting moment time, and smallest box art url
@@ -20,6 +19,16 @@ public class Kata9 {
     public static List<Map> execute() {
         List<MovieList> movieLists = DataUtil.getMovieLists();
 
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "some title", "time", new Date(), "url", "someUrl"));
+        List<Map> video =  movieLists
+                .stream()
+                .flatMap(c ->c.getVideos()
+                        .stream()
+                        .map( x -> ImmutableMap.of("id", x.getId(), "title", x.getTitle(), "time", x.getInterestingMoments().stream().filter(z -> "Middle".equals(z.getType())).map(InterestingMoment::getTime), "boxart", x.getBoxarts().stream()
+                                .reduce((box1, box2) -> box1.getWidth() < box2.getWidth() ? box1 : box2).map(BoxArt::getUrl)
+                                .orElse(""))))
+                .collect(Collectors.toList());
+        System.out.println(video.toString());
+
+        return video;
     }
 }
